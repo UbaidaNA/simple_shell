596043b7b848b4f271f7515531ff8e784c3df38b
Heba Omar <jtvonline@yahoo.com>
2023-05-12 22:06:11 +0300

    a beautiful code that passes the Betty checks

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * main - the entry to the program
+ *
+ * Return: (0)
+ */
+
+
+int main(void)
+{
+
+printf("This is a beautiful code that makes betty proud\n");
+
+return (0);
+}
