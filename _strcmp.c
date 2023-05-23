574b5da60634af8f6bf4bd0bca998ee9d153f8bc
Heba Omar <jtvonline@yahoo.com>
2023-05-23 17:49:01 +0300

    A function that compares strings

---
+++ _strcmp.c
@@ -0,0 +1,18 @@
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: the number of char they don't have in common
+*/
+
+int _strcmp(const char *s1, const char *s2)
+{
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
