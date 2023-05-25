0511ce3a4510ce1ea81940e236844ef4edfb1935
UbaidaNA <ubnapari@gmail.com>
2023-05-25 01:07:44 -0700

    updates

--- handle_path.c
+++ handle_path.c
@@ -9,16 +9,17 @@
 
 void handle_path(const char *command)
 {
-	char path[MAX_PATH_LENGTH];
 	const char *path_env = getenv("PATH");
 	char *path_copy = strdup(path_env);
 	char *token = strtok(path_copy, ":");
 
 	while (token != NULL)
 	{
-		snprintf(path, MAX_PATH_LENGTH, "%s/%s", token, command);
+		char resolved_path[MAX_PATH_LENGTH];
 
-		if (access(path, X_OK) == 0)
+		snprintf(resolved_path, MAX_PATH_LENGTH, "%s/%s", token, command);
+
+		if (access(resolved_path, X_OK) == 0)
 		{
 			break;
 		}
