d41af3f0585cfdf07c7bf6e8d5036096c157ec77
Heba Omar <jtvonline@yahoo.com>
2023-05-25 18:09:30 +0300

    task 1

--- main.c
+++ main.c
@@ -8,43 +8,43 @@
 
 int main(void)
 {
-	char *command = NULL, *token, *args[1024];
-	size_t command_len = 0;
-	int i = 0, status = 0;
-	pid_t pid;
+char *command = NULL, *args[2], *token;
+size_t command_len = 0;
+pid_t pid;
+int status;
 
-	while (printf(" ") && getline(&command, &command_len, stdin) != -1)
-	{
-		for (i = 0, token = strtok(command, " \n");
-				token; token = strtok(NULL, " \n"))
-		{
-			if (token[0] == '#')
-				break;
-			args[i++] = token;
-		}
-		args[i] = NULL;
-		if (args[0] != NULL && _strcmp(args[0], "exit") == 0)
-			break;
-		handle_path(args[0]);
-		if (args[0] != NULL && _strcmp(args[0], "cd") == 0)
-			handle_directory(args);
-		else
-		{
-			pid = fork();
-			if (pid == -1)
-			{
-				perror("fork");
-				exit(0);
-			}
-			else if (pid == 0)
-			{
-				process(pid, args, status);
-				exit(EXIT_SUCCESS);
-			}
-			else
-				waitpid(pid, &status, 0);
-		}
-	}
-	free(command);
-	return (0);
+while (1)
+{
+printf("$ ");
+fflush(stdout);
+if (getline(&command, &command_len, stdin) == -1)
+{
+break;
+}
+if (strcmp(command, "\n") == 0)
+{
+continue;
+}
+token = strtok(command, " \n");
+args[0] = token;
+args[1] = NULL;
+pid = fork();
+if (pid == -1)
+{
+perror("fork");
+exit(0);
+}
+else if (pid == 0)
+{
+execvp(args[0], args);
+perror("Error");
+exit(0);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
+}
+free(command);
+return (0);
 }
